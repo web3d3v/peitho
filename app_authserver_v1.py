4cc9dd74798b7f1f9134f9aa0d95230db0b7e794
shadowyWeb3SuperCoder@protonmail.com <shadowyWeb3SuperCoder@protonmail.com>
2023-05-07 07:45:41 -0300

    removing debug code

--- app_authserver_v1.py
+++ app_authserver_v1.py
@@ -15,8 +15,8 @@ CUSTOMER_API_KEY_SECRET = os.getenv("CUSTOMER_API_KEY_SECRET")
 AUTH_BEARER_TOKEN = os.getenv("AUTH_BEARER_TOKEN")
 WEB3_DEV_ACCESS_TOKEN = os.getenv("WEB3_DEV_ACCESS_TOKEN")
 WEB3_DEV_ACCESS_TOKEN_SECRET = os.getenv("WEB3_DEV_ACCESS_TOKEN_SECRET")
-CLIENT_ID=os.getenv("CLIENT_ID")
-CLIENT_SECRET=os.getenv("CLIENT_SECRET")
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
 oauth1_user_handler = tweepy.OAuth1UserHandler(
     consumer_key=CUSTOMER_API_KEY,
@@ -28,13 +28,15 @@ url = oauth1_user_handler.get_authorization_url()
 print(url)
 
 app = Flask(__name__)
-app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT' # Needed for sessions in flask
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'  # Needed for sessions in flask
+
 
 # Visiting localhost:5000 will redirect to Twitter's app authorization page
 @app.route("/")
 def home():
     session['request_token'] = app.secret_key
     return redirect(url)
+
 
 # If approved you'll be redirected back to localhost:5000/callback, printing the key and secret to the screen
 @app.route("/callback")
@@ -48,12 +50,13 @@ def callback_for_app():
         oauth_verifier
     )
     msg = "\n" + \
-        f"[oauth_token] {oauth_token} \n" + \
-        f"[oauth_verifier] {oauth_verifier} \n" + \
-        f"[access_token] {access_token} \n" + \
-        f"[access_token_secret] {access_token_secret} \n"
+          f"[oauth_token] {oauth_token} \n" + \
+          f"[oauth_verifier] {oauth_verifier} \n" + \
+          f"[access_token] {access_token} \n" + \
+          f"[access_token_secret] {access_token_secret} \n"
     print(msg)
     return msg
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run(ssl_context="adhoc", debug=True)
--- app_authserver_v2.py
+++ app_authserver_v2.py
@@ -13,8 +13,8 @@ CUSTOMER_API_KEY_SECRET = os.getenv("CUSTOMER_API_KEY_SECRET")
 AUTH_BEARER_TOKEN = os.getenv("AUTH_BEARER_TOKEN")
 WEB3_DEV_ACCESS_TOKEN = os.getenv("WEB3_DEV_ACCESS_TOKEN")
 WEB3_DEV_ACCESS_TOKEN_SECRET = os.getenv("WEB3_DEV_ACCESS_TOKEN_SECRET")
-CLIENT_ID=os.getenv("CLIENT_ID")
-CLIENT_SECRET=os.getenv("CLIENT_SECRET")
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
 oauth2_user_handler = tweepy.OAuth2UserHandler(
     client_id=CLIENT_ID,
@@ -27,13 +27,15 @@ url = oauth2_user_handler.get_authorization_url()
 print(url)
 
 app = Flask(__name__)
-app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT' # Needed for sessions in flask
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'  # Needed for sessions in flask
+
 
 # Visiting localhost:5000 will redirect to Twitter's app authorization page
 @app.route("/")
 def home():
     session['request_token'] = app.secret_key
     return redirect(url)
+
 
 # If approved you'll be redirected back to localhost:5000/callback, printing the key and secret to the screen
 @app.route("/callback")
@@ -45,9 +47,10 @@ def callback_for_app():
     user_client = tweepy.Client(access_token["access_token"])
     me = user_client.get_me(user_auth=False)
     print(me)
-    result = user_client.create_tweet(text="Hello world {}".format(datetime.now()), user_auth=False)
-    print(result)
-    return "{}<br/>{}<br/>{}".format(access_token, me, result)
+    # result = user_client.create_tweet(text="Hello world {}".format(datetime.now()), user_auth=False)
+    # print(result)
+    return "{}<br/>{}<br/>{}".format(access_token, me)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run(ssl_context="adhoc", debug=True)
